6a727629d33f7f6e704c441e9f11804cc11801bd
bouwendijk <B.ouwendijk87@gmail.com>
2025-06-21 00:15:32 +0200

    Update klanten.js

--- routes/klanten.js
+++ routes/klanten.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const router = express.Router();
 
+router.get('/', (req, res) => {
+  res.json([
+    { id: 1, naam: 'Jan Jansen' },
+    { id: 2, naam: 'Kees de Vries' },
+  ]);
+});
+
+module.exports = router;
