524d6e107c73e49485557c9aea400fd9375d578d
bouwendijk <B.ouwendijk87@gmail.com>
2025-06-21 00:22:09 +0200

    Update Materiaal.js

--- models/Materiaal.js
+++ models/Materiaal.js
@@ -1,1 +1,14 @@
+const mongoose = require('mongoose');
 
+const materiaalSchema = new mongoose.Schema({
+  naam: {
+    type: String,
+    required: true
+  },
+  voorraad: {
+    type: Number,
+    default: 0
+  }
+});
+
+module.exports = mongoose.model('Materiaal', materiaalSchema);
