df48b34faf4a5acfa5b7077d8b8657613c289558
bouwendijk <B.ouwendijk87@gmail.com>
2025-06-21 00:14:38 +0200

    Update materialen.js

--- routes/materialen.js
+++ routes/materialen.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const router = express.Router();
 
+router.get('/', (req, res) => {
+  res.json([
+    { id: 1, naam: 'PVC buis 20mm' },
+    { id: 2, naam: 'Koppeling 16mm' },
+  ]);
+});
+
+module.exports = router;
